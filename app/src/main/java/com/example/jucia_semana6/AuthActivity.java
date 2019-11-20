d014028d4585c20a6754953601bf04bb024b52ea
unknown <Admin@IHR80PBF451.continental.edu.pe>
2019-11-19 22:30:12 -0500

    decimo commit

--- app/src/main/java/com/example/jucia_semana6/AuthActivity.java
+++ app/src/main/java/com/example/jucia_semana6/AuthActivity.java
@@ -6,23 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class AuthActivity extends AppCompatActivity {
 
 
     Button btnLogin;
-
+    EditText txtUsuario;
+    EditText txtContrasenia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
         btnLogin = findViewById(R.id.btnLogin);
+        txtUsuario = findViewById(R.id.txtUsuario);
+        txtContrasenia = findViewById(R.id.txtcontrasenia);
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i =new Intent(AuthActivity.this,MainActivity.class);
-                startActivity(i);
+                if (txtUsuario.getText().toString().equals("elmer") && txtContrasenia.getText().toString().equals("123456")){
+                    Intent i =new Intent(AuthActivity.this,MainActivity.class);
+                    startActivity(i);
+                }
             }
         });
     }
