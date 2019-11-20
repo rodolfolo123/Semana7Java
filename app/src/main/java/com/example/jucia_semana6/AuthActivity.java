dc98135464d86575bd1df84f765d8d43814f30fe
unknown <Admin@IHR80PBF451.continental.edu.pe>
2019-11-19 22:24:04 -0500

    noveno commit

--- app/src/main/java/com/example/jucia_semana6/AuthActivity.java
+++ app/src/main/java/com/example/jucia_semana6/AuthActivity.java
@@ -2,13 +2,28 @@ package com.example.jucia_semana6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AuthActivity extends AppCompatActivity {
+
+
+    Button btnLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(AuthActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
